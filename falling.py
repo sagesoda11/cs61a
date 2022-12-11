0c5617bc40da7f404978713401d300a46527f0ad
Jessica <85457692+sagesoda11@users.noreply.github.com>
2022-12-11 11:09:20 -0800

    Create falling.py

---
+++ falling.py
@@ -0,0 +1,21 @@
+def falling(n, k):
+    """Compute the falling factorial of n to depth k.
+
+    >>> falling(6, 3)  # 6 * 5 * 4
+    120
+    >>> falling(4, 0)
+    1
+    >>> falling(4, 3)  # 4 * 3 * 2
+    24
+    >>> falling(4, 1)  # 4
+    4
+    """
+    product = 1
+    if k == 0:
+      print(1)
+    for i in range(1,k+1):
+      product = product*(n-i+1)
+      i += 1
+    print(product)
+    
+falling(7,3)
