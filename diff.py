cded59b5c17c3ffcd34c688ce111944ec753f30f
Jessica <85457692+sagesoda11@users.noreply.github.com>
2022-12-11 11:08:08 -0800

    Create diff.py

---
+++ diff.py
@@ -0,0 +1,35 @@
+def diff(x, y, z):
+  """Return whether one argument is the difference between the other two.
+
+    x, y, and z are all integers.
+
+    >>> diff(5, 3, 2) # 5 - 3 is 2
+    True
+    >>> diff(2, 3, 5) # 5 - 3 is 2
+    True
+    >>> diff(2, 5, 3) # 5 - 3 is 2
+    True
+    >>> diff(-2, 3, 5) # 3 - 5 is -2
+    True
+    >>> diff(-5, -3, -2) # -5 - -2 is -3
+    True
+    >>> diff(-2, 3, -5) # -2 - 3 is -5
+    True
+    >>> diff(2, 3, -5)
+    False
+    >>> diff(10, 6, 4)
+    True
+    >>> diff(10, 6, 3)
+    False
+
+    """
+  if x - y == z or y - x == z:
+    return True
+  elif z - y == x or y - z == x:
+    return True
+  elif z - x == y or x - z == y:
+    return True
+  else:
+    return False
+
+print(diff(2,3,-5))
